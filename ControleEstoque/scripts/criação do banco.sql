31086b4d4f7290b7dbc2ceb36bf1059111c8480c
Diego Santos <Diego Santos@DESKTOP-I0EU72F>
2016-04-18 21:45:24 -0300

    Scripts atualizado

--- ControleEstoque/scripts/criação do banco.sql
+++ ControleEstoque/scripts/criação do banco.sql
@@ -15,14 +15,14 @@ CREATE SCHEMA IF NOT EXISTS `controleestoque` DEFAULT CHARACTER SET utf8 ;
 USE `controleestoque` ;
 
 -- -----------------------------------------------------
--- Table `controleestoque`.`almoxarifado`
+-- Table `controleestoque`.`tb_construtora`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `controleestoque`.`almoxarifado` ;
+DROP TABLE IF EXISTS `controleestoque`.`tb_construtora` ;
 
-CREATE TABLE IF NOT EXISTS `controleestoque`.`almoxarifado` (
-  `cd_almoxarifado` INT NOT NULL AUTO_INCREMENT,
-  `resp_almoxarifado` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`cd_almoxarifado`))
+CREATE TABLE IF NOT EXISTS `controleestoque`.`tb_construtora` (
+  `cd_construtora` INT NOT NULL AUTO_INCREMENT,
+  `nm_construtora` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`cd_construtora`))
 ENGINE = InnoDB;
 
 
@@ -36,12 +36,12 @@ CREATE TABLE IF NOT EXISTS `controleestoque`.`tb_obra` (
   `nm_obra` VARCHAR(45) NOT NULL,
   `ds_endereco` VARCHAR(50) NOT NULL,
   `numero` DECIMAL(6) NOT NULL,
-  `cd_almoxarifado` INT NOT NULL,
+  `cd_construtora` INT NOT NULL,
   PRIMARY KEY (`cd_obra`),
-  INDEX `fk_tb_obra_almoxarifado1_idx` (`cd_almoxarifado` ASC),
-  CONSTRAINT `fk_tb_obra_almoxarifado1`
-    FOREIGN KEY (`cd_almoxarifado`)
-    REFERENCES `controleestoque`.`almoxarifado` (`cd_almoxarifado`)
+  INDEX `fk_tb_obra_tb_construtora1_idx` (`cd_construtora` ASC),
+  CONSTRAINT `fk_tb_obra_tb_construtora1`
+    FOREIGN KEY (`cd_construtora`)
+    REFERENCES `controleestoque`.`tb_construtora` (`cd_construtora`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -61,6 +61,25 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
+-- Table `controleestoque`.`almoxarifado`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `controleestoque`.`almoxarifado` ;
+
+CREATE TABLE IF NOT EXISTS `controleestoque`.`almoxarifado` (
+  `cd_almoxarifado` INT NOT NULL AUTO_INCREMENT,
+  `resp_almoxarifado` VARCHAR(45) NOT NULL,
+  `cd_obra` INT NOT NULL,
+  PRIMARY KEY (`cd_almoxarifado`),
+  INDEX `fk_almoxarifado_tb_obra1_idx` (`cd_obra` ASC),
+  CONSTRAINT `fk_almoxarifado_tb_obra1`
+    FOREIGN KEY (`cd_obra`)
+    REFERENCES `controleestoque`.`tb_obra` (`cd_obra`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `controleestoque`.`alocacao_de_material`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `controleestoque`.`alocacao_de_material` ;
@@ -73,6 +92,7 @@ CREATE TABLE IF NOT EXISTS `controleestoque`.`alocacao_de_material` (
   `cd_almoxarifado` INT NOT NULL,
   INDEX `fk_alocacao_de_material_tb_material1_idx` (`cd_material` ASC),
   INDEX `fk_alocacao_de_material_almoxarifado1_idx` (`cd_almoxarifado` ASC),
+  PRIMARY KEY (`cd_almoxarifado`, `cd_material`),
   CONSTRAINT `fk_alocacao_de_material_tb_material1`
     FOREIGN KEY (`cd_material`)
     REFERENCES `controleestoque`.`tb_material` (`cd_material`)
@@ -81,25 +101,6 @@ CREATE TABLE IF NOT EXISTS `controleestoque`.`alocacao_de_material` (
   CONSTRAINT `fk_alocacao_de_material_almoxarifado1`
     FOREIGN KEY (`cd_almoxarifado`)
     REFERENCES `controleestoque`.`almoxarifado` (`cd_almoxarifado`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `controleestoque`.`tb_construtora`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `controleestoque`.`tb_construtora` ;
-
-CREATE TABLE IF NOT EXISTS `controleestoque`.`tb_construtora` (
-  `cd_construtora` INT NOT NULL AUTO_INCREMENT,
-  `nm_construtora` VARCHAR(45) NOT NULL,
-  `cd_obra` INT NOT NULL,
-  PRIMARY KEY (`cd_construtora`),
-  INDEX `fk_tb_construtora_tb_obra1_idx` (`cd_obra` ASC),
-  CONSTRAINT `fk_tb_construtora_tb_obra1`
-    FOREIGN KEY (`cd_obra`)
-    REFERENCES `controleestoque`.`tb_obra` (`cd_obra`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
--- ControleEstoque/scripts/insert do banco.sql
+++ ControleEstoque/scripts/insert do banco.sql
@@ -1,13 +1,21 @@
 use controleestoque;
 
-insert into almoxarifado(resp_almoxarifado) values('Pedro'),('João');
+select * from tb_construtora;
 
-insert into tb_obra(nm_obra,ds_endereco,numero,cd_almoxarifado)
+insert into tb_construtora(nm_construtora) 
+values('C & A Construtora');
+
+select * from tb_obra;
+insert into tb_obra(nm_obra,ds_endereco,numero,cd_construtora)
 values('Corrego do Oleo','Rua do Oleo - Mansour',130,1),
-('Residencial Maravilha','Rua da Maravilha - Maravilha',600,2);
+('Residencial Maravilha','Rua da Maravilha - Maravilha',600,1);
 
-insert into tb_construtora(nm_contrutora,cd_obra) 
-values('C & A Construtora',1);
+select * from almoxarifado;
+
+insert into almoxarifado(resp_almoxarifado,cd_obra) values('Pedro',1),('João',2);
+
+
+select * from tb_material;
 
 insert into tb_material(nm_material,qt_estoque)
 values('Cimento',10),('Cal',10),('Tijolo',50000),
@@ -15,17 +23,19 @@ values('Cimento',10),('Cal',10),('Tijolo',50000),
 ('Cano',100),('Luva',100),('Prego',1000),
 ('Veda Rosca',158),('Lixa',300),('Rolo',100);
 
-insert into alocacao_de_material(cd_almoxarifado,cd_material,qtd_solicitada,dt_solicitacao,nm_solicitante)
-values(1,1,5,2016-04-07,'Roberto'),(1,2,5,2016-04-07,'Roberto'),(1,3,550,2016-04-07,'Roberto'),
-(1,4,1000,2016-04-07,'Roberto'),(1,5,50,2016-04-07,'Roberto'),(1,6,30,2016-04-07,'Roberto'),
-(1,7,1,2016-04-07,'Roberto'),(1,8,3,2016-04-07,'Roberto'),(1,9,50,2016-04-07,'Roberto');
-
+select * from alocacao_de_material;
 
 insert into alocacao_de_material(cd_almoxarifado,cd_material,qtd_solicitada,dt_solicitacao,nm_solicitante)
-values(1,1,9,date_format(07/2016, '%m %y'),'Roberto');
+values(1,1,5,'2016-04-08','Roberto'),(1,2,5,'2016-04-08','Roberto'),(1,3,550,'2016-04-08','Roberto'),
+(1,4,1000,'2016-04-08','Roberto'),(1,5,50,'2016-04-08','Roberto'),(1,6,30,'2016-04-08','Roberto'),
+(1,7,1,'2016-04-08','Roberto'),(1,8,3,'2016-04-08','Roberto'),(1,9,50,'2016-04-08','Roberto');
+
 
 select * from alocacao_de_material;
 select * from tb_material;
+select * from tb_construtora;
+select * from almoxarifado;
+select * from tb_obra;
 
 delete from alocacao_de_material where cd_almoxarifado = 1;
 drop database controleestoque;
